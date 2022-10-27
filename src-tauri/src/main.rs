97468d413e3296033a17e1b8161dda491047a58a
mayzui <ssi_lavarel@163.com>
2022-10-27 22:07:49 +0800

    首次提交

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,109 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+use tauri::Window;
+use std::{thread,time};
+use tauri::Manager;
+
+#[tauri::command]
+fn custom_command(a : f32, b: f32)-> f32 {
+  println!("开始计算:");
+  let c = a * b;
+  c
+}
+
+#[tauri::command]
+fn init_process(window: Window) {
+  std::thread::spawn(move || {
+    loop {
+       window.emit(
+            "event-name",
+            Payload {
+             message: "全局事件!".into()
+            }
+       ).unwrap();
+      thread::sleep(time::Duration::from_millis(500));
+    }
+  });
+}
+
+#[tauri::command]
+
+async fn close_splashscreen(window: tauri::Window) {
+  // Close splashscreen
+  if let Some(splashscreen) = window.get_window("beforeMain") {
+    splashscreen.close().unwrap();
+    window.get_window("main").unwrap().show().unwrap();
+    window.get_window("main").unwrap().set_focus().unwrap();
+  }
+    // Show main window
+}
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  message: String,
+}
+
+
+
+
+
+
+
+use tauri::{SystemTray, SystemTrayMenu,SystemTrayEvent};
+
+fn main() {
+    let tray_menu = SystemTrayMenu::new(); // insert the menu items here
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+
+  tauri::Builder::default()
+    .system_tray(system_tray)
+    .on_system_tray_event(|app, event| match event {
+          SystemTrayEvent::LeftClick {
+            position: _,
+            size: _,
+            ..
+          } => {
+              // println!("点击托盘切换窗口在最小和正常之间切换");
+              let window = app.get_window("main").unwrap();
+              if window.is_visible().unwrap() {
+                  window.hide().unwrap();
+              } else {
+                  window.show().unwrap();
+                  window.set_focus().unwrap();
+              }
+
+          }
+          SystemTrayEvent::RightClick {
+            position: _,
+            size: _,
+            ..
+          } => {
+            println!("right click");
+          }
+          SystemTrayEvent::DoubleClick {
+            position: _,
+            size: _,
+            ..
+          } => {
+            println!("double click");
+          }
+          SystemTrayEvent::MenuItemClick { id, .. } => {
+            match id.as_str() {
+              "quit" => {
+                std::process::exit(0);
+              }
+              "hide" => {
+                let window = app.get_window("main").unwrap();
+                window.hide().unwrap();
+              }
+              _ => {}
+            }
+          }
+          _ => {}
+        })
+    .invoke_handler(tauri::generate_handler![custom_command, init_process,close_splashscreen])
+    .run(tauri::generate_context!())
+    .expect("error while running application");
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,67 @@
+<script setup>
+import {onMounted, ref} from 'vue'
+import { invoke } from '@tauri-apps/api/tauri'
+import { listen } from '@tauri-apps/api/event'
+import { fetch } from '@tauri-apps/api/http';
+import { readBinaryFile, BaseDirectory } from '@tauri-apps/api/fs';
+
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+
+const customFun = ()=>{
+  const a = 0.07
+  const b = 100
+   invoke('custom_command',{a:a,b:b}).then((c)=>{
+     console.log(c)
+   })
+
+}
+
+const initProcess = async ()=>{
+  await invoke('init_process').then((res)=>{
+    console.log(res)
+  });
+
+   await listen('event-name', (event) => {
+    console.log(event);
+  });
+
+}
+
+const getWeather = async ()=>{
+  const response = await fetch('https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&ext=&cityid=&city=%E9%87%8D%E5%BA%86', {
+    method: 'GET',
+    timeout: 30,
+  }).then((res)=>{
+    console.log(res)
+  });
+}
+
+const readImg = async  ()=>{
+  const contents = await readBinaryFile('./images/32x32.png', { dir: BaseDirectory.Resource });
+  console.log(contents)
+}
+
+onMounted(()=>{
+  setTimeout(function(){
+    invoke('close_splashscreen');
+  },5000)
+})
+</script>
+
+<template>
+<button @click="customFun">要调用rust方法</button>
+<button @click="initProcess">开启事件</button>
+<button @click="getWeather">获取天气</button>
+<button @click="readImg">读取图片</button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
